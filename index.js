5d226aaa13e6ea310aef6a1f2649dddf688c1014
Sarel <sknoetze5@gmail.com>
2023-04-10 12:56:15 +0200

    Create

---
+++ index.js
@@ -0,0 +1,106 @@
+document.getElementById("gd").onclick = function(){
+    location.href = "#form";
+}
+
+document.getElementById("gdd").onclick = function(){
+    location.href = "#form";
+}
+
+document.getElementById("kkOne").onclick = function(){
+    document.getElementById("one").style.cssText = "display: block;"
+    document.getElementById("koOne").style.cssText = "display: inline;"
+    document.getElementById("kkOne").style.cssText = "display: none;"
+}
+document.getElementById("koOne").onclick = function(){
+    document.getElementById("one").style.cssText = "display: none;"
+    document.getElementById("koOne").style.cssText = "display: none;"
+    document.getElementById("kkOne").style.cssText = "display: inline;"
+}
+
+document.getElementById("kkTwo").onclick = function(){
+    document.getElementById("two").style.cssText = "display: block;"
+    document.getElementById("koTwo").style.cssText = "display: inline;"
+    document.getElementById("kkTwo").style.cssText = "display: none;"
+}
+document.getElementById("koTwo").onclick = function(){
+    document.getElementById("two").style.cssText = "display: none;"
+    document.getElementById("koTwo").style.cssText = "display: none;"
+    document.getElementById("kkTwo").style.cssText = "display: inline;"
+}
+
+document.getElementById("kkThree").onclick = function(){
+    document.getElementById("Three").style.cssText = "display: block;"
+    document.getElementById("koThree").style.cssText = "display: inline;"
+    document.getElementById("kkThree").style.cssText = "display: none;"
+}
+document.getElementById("koThree").onclick = function(){
+    document.getElementById("Three").style.cssText = "display: none;"
+    document.getElementById("koThree").style.cssText = "display: none;"
+    document.getElementById("kkThree").style.cssText = "display: inline;"
+}
+
+document.getElementById("kkFour").onclick = function(){
+    document.getElementById("four").style.cssText = "display: block;"
+    document.getElementById("koFour").style.cssText = "display: inline;"
+    document.getElementById("kkFour").style.cssText = "display: none;"
+}
+document.getElementById("koFour").onclick = function(){
+    document.getElementById("four").style.cssText = "display: none;"
+    document.getElementById("koFour").style.cssText = "display: none;"
+    document.getElementById("kkFour").style.cssText = "display: inline;"
+}
+
+document.getElementById("kkFive").onclick = function(){
+    document.getElementById("five").style.cssText = "display: block;"
+    document.getElementById("koFive").style.cssText = "display: inline;"
+    document.getElementById("kkFive").style.cssText = "display: none;"
+}
+document.getElementById("koFive").onclick = function(){
+    document.getElementById("five").style.cssText = "display: none;"
+    document.getElementById("koFive").style.cssText = "display: none;"
+    document.getElementById("kkFive").style.cssText = "display: inline;"
+}
+
+document.getElementById("kkSix").onclick = function(){
+    document.getElementById("six").style.cssText = "display: block;"
+    document.getElementById("koSix").style.cssText = "display: inline;"
+    document.getElementById("kkSix").style.cssText = "display: none;"
+}
+document.getElementById("koSix").onclick = function(){
+    document.getElementById("six").style.cssText = "display: none;"
+    document.getElementById("koSix").style.cssText = "display: none;"
+    document.getElementById("kkSix").style.cssText = "display: inline;"
+}
+
+document.getElementById("kkSeven").onclick = function(){
+    document.getElementById("seven").style.cssText = "display: block;"
+    document.getElementById("koSeven").style.cssText = "display: inline;"
+    document.getElementById("kkSeven").style.cssText = "display: none;"
+}
+document.getElementById("koSeven").onclick = function(){
+    document.getElementById("seven").style.cssText = "display: none;"
+    document.getElementById("koSeven").style.cssText = "display: none;"
+    document.getElementById("kkSeven").style.cssText = "display: inline;"
+}
+
+document.getElementById("kkEight").onclick = function(){
+    document.getElementById("eight").style.cssText = "display: block;"
+    document.getElementById("koEight").style.cssText = "display: inline;"
+    document.getElementById("kkEight").style.cssText = "display: none;"
+}
+document.getElementById("koEight").onclick = function(){
+    document.getElementById("eight").style.cssText = "display: none;"
+    document.getElementById("koEight").style.cssText = "display: none;"
+    document.getElementById("kkEight").style.cssText = "display: inline;"
+}
+
+document.getElementById("kkNine").onclick = function(){
+    document.getElementById("nine").style.cssText = "display: block;"
+    document.getElementById("koNine").style.cssText = "display: inline;"
+    document.getElementById("kkNine").style.cssText = "display: none;"
+}
+document.getElementById("koNine").onclick = function(){
+    document.getElementById("nine").style.cssText = "display: none;"
+    document.getElementById("koNine").style.cssText = "display: none;"
+    document.getElementById("kkNine").style.cssText = "display: inline;"
+}
